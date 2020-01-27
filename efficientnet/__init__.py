aae16d92f9f2e9e13f260d5f4eff37e9161cb33d
Changyu Liu <liuchangyu1111@gmail.com>
2020-01-27 13:22:37 +0800

    Incremeneted version

--- efficientnet/__init__.py
+++ efficientnet/__init__.py
@@ -1,10 +1,7 @@
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 from .model import EfficientNet
-from .utils import (
-    GlobalParams,
-    BlockArgs,
-    BlockDecoder,
-    efficientnet,
-    get_model_params,
-)
-
+from .utils import GlobalParams
+from .utils import BlockArgs
+from .utils import BlockDecoder
+from .utils import efficientnet
+from .utils import get_model_params
